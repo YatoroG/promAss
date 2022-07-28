07f5b2677de7b6ecac3b96330a8f78dba913cf9e
YatoroG <endlessly.space.com@gmail.com>
2022-07-28 17:59:56 +0300

    feat[serialconnection]: попытка написать функции класса serialconnection

--- src/dao/SerialConnection.cs
+++ src/dao/SerialConnection.cs
@@ -4,24 +4,60 @@ using System.IO.Ports;
 public class SerialConnection : IConnectable
 {
     private SerialPort port;
-
+    string name;
     public void Connect()
     {
-
+        try
+        {
+            port.PortName = name;
+            port.Open();
+            Console.WriteLine("Connection is opened");
+        }
+        catch
+        {
+            Console.WriteLine("An error occurred");
+        }
     }
 
     public void Disconnect()
     {
-
+        try
+        {
+            port.PortName = name;
+            port.Close();
+            Console.WriteLine("Connection is closed");
+        }
+        catch
+        {
+            Console.WriteLine("An error occurred");
+        }
     }
 
     public object ReadData()
     {
-        return null;
+        object obj; 
+
+        try
+        {
+            port.PortName = name;
+            obj = port.ReadLine();
+            return obj;
+        }
+        catch
+        {
+            return null;
+        }
     }
 
     public void SendMessage(object message)
     {
-
+        if (message is null)
+        {
+            Console.WriteLine("Message is empty");
+        }
+        else
+        {
+            port.WriteLine(String.Format("{0}: {1}", name, message));
+        }
     }
 }
