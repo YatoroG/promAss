94cc544cfebece819973bd52d1e5fe0294eca5b4
0xABADBEEF <romkepermyakov@yandex.ru>
2022-07-28 10:20:05 +0300

    feat[SerialConnection]: добавлена начальная реализация

---
+++ dao/SerialConnection.cs
@@ -0,0 +1,27 @@
+using System;
+using System.IO.Ports;
+
+class SerialConnection : IConnectable
+{
+    private SerialPort port;
+
+    public void Connect()
+    {
+
+    }
+
+    public void Disconnect()
+    {
+
+    }
+
+    public object ReadData()
+    {
+        return null;
+    }
+
+    public void SendMessage(object message)
+    {
+
+    }
+}
